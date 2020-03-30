a18c1cab59d0fb8f8f2b718793f01b51aa8c4cc9
Akash Kumar <aksh.iem22@gmail.com>
2020-03-30 16:47:19 +0530

    Added Template

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
-
 const path = require('path');
-
 const app = express();
-
-app.use(express.static(path.join(__dirname, 'public')));
+const handlebars = require('express-handlebars');
 
 const PORT = process.env.PORT || 5000;
+
+app.set('view engine', 'handlebars');
+
+
+app.engine('handlebars', handlebars({
+layoutsDir: __dirname + '/views/layouts',
+}));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('main', {layout: 'index'});
+});
+
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
