a9174c86b1097f818530278982b7c22f5ad11d02
shivishbrahma <pur.cho.99@gmail.com>
2020-04-19 02:19:52 +0530

    Added Quiz Utility

--- public/js/master.js
+++ public/js/master.js
@@ -1,8 +1,46 @@
+const options = {
+  toolbar: [
+    ['style', ['bold', 'italic', 'underline', 'clear']],
+    ['font', ['strikethrough', 'superscript', 'subscript']],
+    ['fontsize', ['fontsize']],
+    ['color', ['color']],
+    ['para', ['ul', 'ol', 'paragraph']],
+    ['insert', ['link', 'picture', 'video']],
+    ['view', ['fullscreen', 'codeview', 'help']],
+  ],
+};
+
 (($) => {
-  $('input[name="answer"]:checked')
-    .parent()
-    .parent()
-    .css('background-color', '#6610f2 !important');
+  // Disable Right Click
+  $(document).on('contextmenu', function () {
+    return false;
+  });
+
+  let index = 0,
+    length = $('.quizForm .quiz').length;
+
+  $('.quizForm .quiz').addClass('d-none');
+  $($('.quizForm .quiz').get(index)).removeClass('d-none');
+
+  $('.quizForm #submit').click(function (e) {
+    correct = $('.quizForm .quiz input[type="radio"]:checked.correct').length;
+    $('.modal-body').get(0).innerHTML = `Your Score is ${correct} out of 5`;
+  });
+  $('.quizForm #prev').click(function (e) {
+    if (index != 0) index--;
+    $('.quizForm .quiz').addClass('d-none');
+    $($('.quizForm .quiz').get(index)).removeClass('d-none');
+  });
+  $('.quizForm #next').click(function (e) {
+    if (index != length - 1) index++;
+    $('.quizForm .quiz').addClass('d-none');
+    $($('.quizForm .quiz').get(index)).removeClass('d-none');
+  });
+
+  // $('input[name^="answer"]:checked')
+  //   .parent()
+  //   .parent()
+  //   .css('background-color', '#6610f2 !important');
   $('#nopt').on('input', function (e) {
     const answer = $('#answer').get(0),
       optList = $('#option-list').get(0);
@@ -16,27 +54,10 @@
     }
     optList.innerHTML = olhtml;
     answer.innerHTML = ohtml;
-    $('.summernote').summernote();
+    $('.summernote').summernote(options);
   });
 })(jQuery);
 
 $(document).ready(function () {
-  // const answer = $('#answer').get(0),
-  //   optList = $('#option-list').get(0);
-  // let ohtml = '',
-  //   olhtml = '',
-  //   n = $('#nopt').value;
-
-  // for (let i = 0; i < n; i++) {
-  //   olhtml += `<div class="form-group">
-  //   <label for="option[${i}]">Option${i + 1}</label>
-  //   <textarea class="summernote" name="option[${i}]" rows="5" placeholder="Option${
-  //     i + 1
-  //   }"></textarea></div>`;
-  //   ohtml = ohtml + `<option value="${i + 1}">${i + 1}</option>`;
-  // }
-  // optList.innerHTML = olhtml;
-  // answer.innerHTML = ohtml;
-
-  $('.summernote').summernote();
+  $('.summernote').summernote(options);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-// const Story = mongoose.model('stories');
+const Question = mongoose.model('quizes');
+const Topic = mongoose.model('topics');
 const { ensureAuthenticated, ensureGuest } = require('../helpers/auth');
+const selectFactory = require('random-selector');
+
+randomSelect = (list, number) => {
+  normalBag = selectFactory.createSimpleSelectorWithoutReplacement(list);
+  selection = Array();
+  for (i = 0; i < number; i++) {
+    selection.push(normalBag.select());
+  }
+  return selection;
+};
 
 router.get('/', ensureGuest, (req, res) => {
-  res.render('./index/welcome');
+  res.render('index/welcome');
 });
 
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
   res.render('index/dashboard');
 });
 
+router.get('/see/topics', ensureAuthenticated, (req, res) => {
+  Topic.find().then((topic) => {
+    res.render('index/topics', { topic: topic });
+  });
+});
+
+router.get('/see/quiz/:id', ensureAuthenticated, (req, res) => {
+  Question.find({ category: req.params.id })
+    .populate('category')
+    .then((quiz) => {
+      selectedQuiz = randomSelect(quiz, 5);
+      res.render('index/quiz', { quiz: selectedQuiz });
+    });
+});
 module.exports = router;
