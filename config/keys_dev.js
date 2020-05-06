92cfe993037f6983baa5163948bf88bdcf2f5e10
shivishbrahma <pur.cho.99@gmail.com>
2020-05-06 12:38:10 +0530

    Minor Change

--- config/keys_dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mongoURI:
-    'mongodb+srv://mlxplorer:PT8txgN5LYdQandO@datafier-4c5tk.mongodb.net/dataquiz?retryWrites=true&w=majority',
-  googleClientID:
-    '71316863667-pclotg89f8ci5rio7a0agh3nfojb98sd.apps.googleusercontent.com',
-  googleClientSecret: '79YhEnMrq0zlR7qNxhddlOr4',
-};
